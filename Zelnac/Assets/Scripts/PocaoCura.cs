d574712e51182d911b2e48733bc47130be68b662
lostada <arthurlostada0@gmail.com>
2025-04-01 17:18:19 -0300

    bola de fogo

---
+++ Zelnac/Assets/Scripts/PocaoCura.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PocaoCura : MonoBehaviour
+{
+    public int cura = 1; // A poção recupera 1 coração
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        PlayerStatus player = other.GetComponent<PlayerStatus>();
+
+        if (player != null)
+        {
+            player.Curar(cura);
+            Destroy(gameObject);
+        }
+    }
+}
